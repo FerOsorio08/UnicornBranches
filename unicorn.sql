4544bbde1929954b282e873422a6d9150c3fc9bf
fernicortlo <a01026613@tec.mx>
2023-03-30 12:24:44 -0600

    sql

--- unicorn.sql
+++ unicorn.sql
@@ -1,1 +1,8 @@
 -- Creation 
+CREATE TABLE unicorn (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    color TEXT NOT NULL,
+    horn_length INTEGER NOT NULL,
+    age INTEGER NOT NULL
+);
